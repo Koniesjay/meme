c8f86fba449d4faa6c288cb7335b4d62ca248135
Koniesjay <jkonyeri@gmail.com>
2024-03-18 13:38:49 +0100

    first commit

--- contract.sol
+++ contract.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract DegenGamingToken is ERC20 {
+contract MEME is ERC20 {
     address public owner;
 
     constructor() ERC20("MEME", "MME") {
